cfff34c5e19b4eeadca49874b2f25d7914153f53
Mike Sardonini <sardonini.michael@aurora.aero>
2022-07-28 23:09:35 -0700

    deleted unused code, added comments

--- src/oakd_s2/src/oakd_ros_publisher.cpp
+++ src/oakd_s2/src/oakd_ros_publisher.cpp
@@ -11,23 +11,21 @@
 // #include "oakd_s2/opencv_adapter.h"
 #include "cv_bridge/cv_bridge.h"
 
-static std::atomic<bool> lrcheck{true};
-static std::atomic<bool> extended{false};
-static std::atomic<bool> subpixel{false};
 static constexpr auto timeout_period = std::chrono::milliseconds(1000);
-static constexpr auto stereo_sync_time_threshold_us =
-    25;  //< If left/right images have a timestamp difference of more than this, we will
-         // assume that the images are not synchronized.
+static const std::string imu_topic_name = "/visual_slam/imu";
+static const std::string cam0_topic_name = "/stereo_camera/left/image";
+static const std::string cam0_info_topic_name = "/stereo_camera/left/camera_info";
+static const std::string cam1_topic_name = "/stereo_camera/right/image";
+static const std::string cam1_info_topic_name = "/stereo_camera/right/camera_info";
 
+/**
+ * @brief Pimpl object to hold the dai::rosBridge objects. This is a pimpl because several functions in dai::rosBridge's
+ * header file are not inlined and therefore cannot be compiled in multiple translation units.
+ */
 class Pimpl {
  public:
   std::unique_ptr<dai::rosBridge::ImuConverter> imu_converter;
   std::unique_ptr<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Imu, dai::IMUData>> imu_publisher;
-
-  std::unique_ptr<dai::rosBridge::ImageConverter> left_converter;
-  std::unique_ptr<dai::rosBridge::ImageConverter> right_converter;
-  std::unique_ptr<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame>> left_publisher;
-  std::unique_ptr<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image, dai::ImgFrame>> right_publisher;
 };
 
 OakdRosPublisher::OakdRosPublisher(std::shared_ptr<rclcpp::Node> node, std::optional<StereoCalibration> calibration)
@@ -58,16 +56,13 @@ void OakdRosPublisher::spin() { rclcpp::spin(node_); }
 
 void OakdRosPublisher::Init() {
   // Initialize ROS publishers
-  publisher_left_image_ = node_->create_publisher<sensor_msgs::msg::Image>("/stereo_camera/left/image", 1);
-  publisher_left_cam_info_ =
-      node_->create_publisher<sensor_msgs::msg::CameraInfo>("/stereo_camera/left/camera_info", 1);
-  publisher_right_image_ = node_->create_publisher<sensor_msgs::msg::Image>("/stereo_camera/right/image", 1);
-  publisher_right_cam_info_ =
-      node_->create_publisher<sensor_msgs::msg::CameraInfo>("/stereo_camera/right/camera_info", 1);
-
-  start_time_ = std::chrono::steady_clock::now();
+  publisher_left_image_ = node_->create_publisher<sensor_msgs::msg::Image>(cam0_topic_name, 1);
+  publisher_left_cam_info_ = node_->create_publisher<sensor_msgs::msg::CameraInfo>(cam0_info_topic_name, 1);
+  publisher_right_image_ = node_->create_publisher<sensor_msgs::msg::Image>(cam1_topic_name, 1);
+  publisher_right_cam_info_ = node_->create_publisher<sensor_msgs::msg::CameraInfo>(cam1_info_topic_name, 1);
 
   is_running_ = true;
+  start_time_ = std::chrono::steady_clock::now();
   publish_stereo_image_thread_ = std::thread(&OakdRosPublisher::publish_stereo_image_thread, this);
   publish_imu_thread_ = std::thread(&OakdRosPublisher::publish_imu_thread, this);
 }
@@ -80,111 +75,7 @@ static auto get_ts_now(
   return rclNow - diffTime;
 }
 
-auto get_camera_calibration(dai::CalibrationHandler &calibration_handler, dai::CameraBoardSocket socket,
-                            cv::Size imsize) {
-  auto cam0_intrinsics_dai =
-      calibration_handler.getCameraIntrinsics(socket, std::make_tuple(imsize.width, imsize.height));
-  std::array<double, 9> cam0_intrinsics_ros;
-
-  std::size_t ind = 0;
-  std::for_each(cam0_intrinsics_dai.begin(), cam0_intrinsics_dai.end(), [&](auto val) {
-    std::for_each(val.begin(), val.end(), [&](auto val) {
-      if (ind >= 9) {
-        throw std::runtime_error("Invalid camera intrinsics");
-      }
-      cam0_intrinsics_ros[ind++] = val;
-    });
-  });
-
-  auto cam0_distortion_dai = calibration_handler.getDistortionCoefficients(socket);
-
-  std::vector<double> cam0_distortion_ros;
-  std::for_each(cam0_distortion_dai.begin(), cam0_distortion_dai.end(),
-                [&](auto val) { cam0_distortion_ros.push_back(val); });
-
-  return std::make_pair(cam0_intrinsics_ros, cam0_distortion_ros);
-}
-
-template <bool output_opencv_type>
-auto calculate_projection_matrices(std::array<double, 9> K0, std::vector<double> D0, std::array<double, 9> K1,
-                                   std::vector<double> D1, cv::Size imsize, std::array<double, 9> R,
-                                   std::array<double, 3> T) {
-  cv::Mat_<double> K0_cv = cv::Mat_<double>(3, 3, K0.data());
-  cv::Mat_<double> D0_cv = cv::Mat_<double>(D0.size(), 1, D0.data());
-  cv::Mat_<double> K1_cv = cv::Mat_<double>(3, 3, K1.data());
-  cv::Mat_<double> D1_cv = cv::Mat_<double>(D1.size(), 1, D1.data());
-  cv::Mat_<double> R_cv = cv::Mat_<double>(3, 3, R.data());
-  cv::Mat_<double> T_cv = cv::Mat_<double>(3, 1, T.data());
-
-  cv::Mat_<double> R0_cv, R1_cv;
-  cv::Mat_<double> P0_cv, P1_cv;
-  cv::Mat_<double> Q_cv;
-  cv::Rect valid_roi_0, valid_roi_1;
-
-  cv::stereoRectify(K0_cv, D0_cv, K1_cv, D1_cv, imsize, R_cv, T_cv, R0_cv, R1_cv, P0_cv, P1_cv, Q_cv,
-                    cv::CALIB_ZERO_DISPARITY, -1, cv::Size(), &valid_roi_0, &valid_roi_1);
-
-  std::cout << "valid_roi_0: " << valid_roi_0 << std::endl;
-  std::cout << "valid_roi_1: " << valid_roi_1 << std::endl;
-
-  if constexpr (output_opencv_type) {
-    return std::make_tuple(R0_cv, R1_cv, P0_cv, P1_cv, Q_cv, valid_roi_0, valid_roi_1);
-  } else {
-    // Convert the Projection matrices back to arrays
-    auto conv_func = [](auto &dst, cv::Mat_<double> &src) {
-      std::memcpy(dst.data(), src.data, src.elemSize() * src.total());
-    };
-    std::array<double, 12> P0, P1;
-    conv_func(P0, P0_cv);
-    conv_func(P1, P1_cv);
-
-    return std::make_tuple(P0, P1);
-  }
-}
-
 void OakdRosPublisher::publish_stereo_image_thread() {
-  // auto calibration_handler = device_->readCalibration();
-
-  // pimpl_->left_converter =
-  // std::make_unique<dai::rosBridge::ImageConverter>("odom", true);
-  // pimpl_->right_converter =
-  // std::make_unique<dai::rosBridge::ImageConverter>("odom", true);
-
-  // int mono_width = 1280;
-  // int mono_height = 720;
-  // auto left_camera_info =
-  // pimpl_->left_converter->calibrationToCameraInfo(calibration_handler,
-  // dai::CameraBoardSocket::LEFT, mono_width, mono_height); auto
-  // right_camera_info =
-  // pimpl_->right_converter->calibrationToCameraInfo(calibration_handler,
-  // dai::CameraBoardSocket::RIGHT, mono_width, mono_height);
-
-  // const std::string left_pub_name("/stereo_camera/left/image");
-  // const std::string right_pub_name("/stereo_camera/right/image");
-  // pimpl_->left_publisher =
-  // std::make_unique<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image,
-  // dai::ImgFrame>>(
-  //     queue_left_rect_,
-  //     node_,
-  //     left_pub_name,
-  //     std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
-  //     pimpl_->left_converter.get(), std::placeholders::_1,
-  //     std::placeholders::_2), 3, left_camera_info, "stereo_camera/left");
-
-  // pimpl_->right_publisher =
-  // std::make_unique<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Image,
-  // dai::ImgFrame>>(
-  //     queue_right_rect_,
-  //     node_,
-  //     right_pub_name,
-  //     std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
-  //     pimpl_->right_converter.get(), std::placeholders::_1,
-  //     std::placeholders::_2), 3, right_camera_info, "stereo_camera/right");
-  // pimpl_->left_publisher->addPublisherCallback();
-  // pimpl_->right_publisher->addPublisherCallback();
-
-  // UNCOMMENT BELOW TO PUBLISH STEREO IMAGES
-
   while (is_running_.load()) {
     bool has_timed_out = false;
     auto stereo_pair = GetStereoImagePair(timeout_period, has_timed_out);
@@ -224,42 +115,9 @@ void OakdRosPublisher::publish_imu_thread() {
       std::make_unique<dai::rosBridge::ImuConverter>("oak_imu", imu_mode, linear_accel_cov, angular_vel_cov);
 
   pimpl_->imu_publisher = std::make_unique<dai::rosBridge::BridgePublisher<sensor_msgs::msg::Imu, dai::IMUData>>(
-      queue_imu_, node_, std::string("/visual_slam/imu"),
+      queue_imu_, node_, std::string(imu_topic_name),
       std::bind(&dai::rosBridge::ImuConverter::toRosMsg, pimpl_->imu_converter.get(), std::placeholders::_1,
                 std::placeholders::_2),
       30, "", "imu");
   pimpl_->imu_publisher->addPublisherCallback();
-
-  // while (is_running_.load()) {
-
-  //   bool has_timed_out;
-  //   auto imu_data = GetImuData(timeout_period, has_timed_out);
-
-  //   if (has_timed_out) {
-  //     std::cerr << "timed out getting imu data" << std::endl;
-
-  //     // Sleep so we don't spam the system with polls
-  //     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  //     continue;
-  //   }
-
-  //   for (auto &packet : imu_data) {
-  //     auto time_since_start_ns =
-  //     std::chrono::duration_cast<std::chrono::nanoseconds>(packet.gyroscope.timestamp.get()
-  //     - start_time_).count(); std_msgs::msg::Header header;
-  //     header.stamp.nanosec = time_since_start_ns % 1000000000;
-  //     header.stamp.sec = time_since_start_ns / 1000000000;
-  //     header.frame_id = "imu";
-
-  //     sensor_msgs::msg::Imu imu_msg;
-  //     imu_msg.header = header;
-  //     imu_msg.angular_velocity.x = packet.gyroscope.x;
-  //     imu_msg.angular_velocity.y = packet.gyroscope.y;
-  //     imu_msg.angular_velocity.z = packet.gyroscope.z;
-  //     imu_msg.linear_acceleration.x = packet.acceleroMeter.x;
-  //     imu_msg.linear_acceleration.y = packet.acceleroMeter.y;
-  //     imu_msg.linear_acceleration.z = packet.acceleroMeter.z;
-  //     publisher_imu_->publish(imu_msg);
-  //   }
-  // }
 }
